f41d9fbf0a6a3e1303f6ddcd9d67a039a1c9edf8
Ashok Pudasaini <67636572+ashokpds15@users.noreply.github.com>
2026-03-11 06:37:01 +0545

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,95 +15,114 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
   return (
-    <section>
-      <Container fluid className="home-section" id="home" style={{ backgroundImage: `url(${homeBg})`, backgroundSize: "cover", backgroundPosition: "center" }}>
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+    <>
+      {/* ----------------------------
+          1️⃣ NEPSE Ticker at the top
+      ---------------------------- */}
+      <NEPSETicker />
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Ashok Pudasaini</strong>
-              </h1>
+      {/* ----------------------------
+          2️⃣ Existing Home Section
+      ---------------------------- */}
+      <section>
+        <Container
+          fluid
+          className="home-section"
+          id="home"
+          style={{
+            backgroundImage: `url(${homeBg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Particle />
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Hi There!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
+                    👋🏻
+                  </span>
+                </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+                <h1 className="heading-name">
+                  I'M
+                  <strong className="main-name"> Ashok Pudasaini</strong>
+                </h1>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+                <div style={{ padding: 50, textAlign: "left" }}>
+                  <Type />
+                </div>
+              </Col>
+
+              <Col md={5} style={{ paddingBottom: 20 }}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+        <Home2 />
+
+        <Container>
+          <Row style={{ paddingTop: "50px", paddingBottom: "80px" }}>
+            <Col md={12} className="home-about-social">
+              <h1>Find Me On</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/ashokpds15"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://twitter.com/ashokpds"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiOutlineTwitter />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/ashok-pudasaini-2560a762/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/aadit_chhetri"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
             </Col>
           </Row>
         </Container>
-      </Container>
-      <Home2 />
-
-      <Container>
-        <Row style={{ paddingTop: "50px", paddingBottom: "80px" }}>
-          <Col md={12} className="home-about-social">
-            <h1>Find Me On</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/ashokpds15"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/ashokpds"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/ashok-pudasaini-2560a762/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/aadit_chhetri"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+      </section>
+    </>
   );
 }
 
